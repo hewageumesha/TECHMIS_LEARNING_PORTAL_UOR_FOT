b0dc6a74acdadebb21703ef58a4654cb7d1b1504
Nethmi Umesha <n.u.m.hewage@gmail.com>
2024-05-07 15:58:49 +0530

    w

---
+++ src/OfficerModule/TableManagement.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package OfficerModule;
+
+public interface TableManagement {
+     void Reset();
+     void Insert();
+     void SetValue();
+     void Referance();
+     void ShowTable();
+     void Update();
+     
+    
+}
